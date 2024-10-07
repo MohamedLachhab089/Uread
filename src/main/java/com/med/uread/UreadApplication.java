9436b182b8bef709323325e023e1bb84c6d0bc65
Mohamed <mohamed.lachhab@emsi-edu.ma>
2024-10-07 21:07:20 +0100

    first commit

---
+++ src/main/java/com/med/uread/UreadApplication.java
@@ -0,0 +1,13 @@
+package com.med.uread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/med/uread/UreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.uread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
